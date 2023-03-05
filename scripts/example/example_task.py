fe9f7c2afa98be5894e90c051243cd2dfaab1e12
Billy <wtr2@st-andrews.ac.uk>
2023-03-05 08:58:51 +0000

    Move example

--- scripts/example/example_task.py
+++ scripts/example/example_task.py
@@ -5,7 +5,7 @@ from jhcompute.task_utils import load_input, write_output
 parameters = load_input()
 
 random_list = [random.randint(0, parameters["size"]) for _ in range(parameters["size"])]
-random_list.sort()[2.5]
+random_list.sort()
 
 write_output({
     "result": random_list
