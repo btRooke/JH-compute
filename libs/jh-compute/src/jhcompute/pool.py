41004e4e9dc48085dbb784af8a63b0d1ff6767ee
Billy <wtr2@st-andrews.ac.uk>
2023-03-05 00:00:49 +0000

    Comments

--- libs/jh-compute/src/jhcompute/pool.py
+++ libs/jh-compute/src/jhcompute/pool.py
@@ -14,14 +14,19 @@ from jhcompute.searching import active_nodes_user_count
 
 class JHPool:
 
-    def get_next_task_id(self):
-
-        with self.task_id_lock:
-            to_return = self.task_id
-            self.task_id += 1
-            return to_return
-
     def __init__(self, node_count: int, job_file: str) -> None:
+
+        """
+        Create a pool of workers which can concurrently run a job script in the JH labs.
+
+        The task script can read a JSON object (the task object) of parameters
+        from the path to a temporary file provided at argv[1].
+
+        A response should be written to this same file once complete.
+
+        :param node_count: The amount of nodes (lab machines) to use.
+        :param job_file: A path (relative to the CWD) for a task script.
+        """
 
         self.task_id_lock = Lock()
         self.task_id = 0
@@ -53,7 +58,7 @@ class JHPool:
         potential_nodes = active_nodes_user_count()
 
         logging.info(
-            f"Found {len(potential_nodes)} potential active nodes -- {len([n for n in potential_nodes if n[1] == 0])} have no users"
+            f"Found {len(potential_nodes)} potential active nodes -- require {self.node_count} -- {len([n for n in potential_nodes if n[1] == 0])} have no users"
         )
 
         if len(potential_nodes) < self.node_count:
@@ -62,7 +67,7 @@ class JHPool:
         chosen_nodes = potential_nodes[:self.node_count]
 
         for node in filter(lambda n: n[1] > 0, chosen_nodes):
-            logging.warning(f"{node[0]} already has {node[1]} user(s)...")
+            logging.warning(f"{node[0]} already have {node[1]} user(s)...")
 
         self.nodes = [Node(self, node[0]) for node in chosen_nodes]
         self.free_nodes = queue.Queue()
@@ -77,21 +82,24 @@ class JHPool:
         self.potential_nodes = queue.Queue()
         [self.potential_nodes.put(node[0]) for node in potential_nodes[self.node_count:]]
 
-    def write_to_temp(self, json_object: dict):
-
-        filename = self.temp_dir + f"{''.join(random.choices(string.ascii_letters, k=32))}.json"
-
-        while Path(filename).is_file():
-            filename = self.temp_dir + f"{''.join(random.choices(string.ascii_letters, k=32))}.json"
-
-        with open(filename, "w") as file:
-            json.dump(json_object, file)
-
-        return filename
-
     def submit(self, task_object: dict) -> dict:
 
-        task_id = self.get_next_task_id()
+        """
+        Submit a task object to be processed by the pool task script.
+
+        Blocks until complete.
+
+        Handles node failure by replacing the node -- fails if all nodes in JH
+        are used up (this is very unlikely).
+
+        Throws a RuntimeException if the script being run on some worker node
+        fails.
+
+        :param task_object: A JSON object (python dict) of parameters for the task script to process.
+        :return: A python dict -- the response of the task script.
+        """
+
+        task_id = self._get_next_task_id()
 
         logging.info(f"Task {task_id} submitted -- {task_object}")
         node = self.free_nodes.get()
@@ -106,7 +114,7 @@ class JHPool:
 
             except NodeDiedException:
                 logging.warning(f"Node died during {task_id} - {node.hostname}")
-                node = self.new_node()
+                node = self._new_node()
                 logging.info(f"Selected {node.hostname} as new node!")
 
         self.free_nodes.put(node)
@@ -115,7 +123,26 @@ class JHPool:
 
         return result
 
-    def new_node(self):
+    def _get_next_task_id(self):
+
+        with self.task_id_lock:
+            to_return = self.task_id
+            self.task_id += 1
+            return to_return
+
+    def write_to_temp(self, json_object: dict):
+
+        filename = self.temp_dir + f"{''.join(random.choices(string.ascii_letters, k=32))}.json"
+
+        while Path(filename).is_file():
+            filename = self.temp_dir + f"{''.join(random.choices(string.ascii_letters, k=32))}.json"
+
+        with open(filename, "w") as file:
+            json.dump(json_object, file)
+
+        return filename
+
+    def _new_node(self):
         return Node(self, self.potential_nodes.get())
 
 
