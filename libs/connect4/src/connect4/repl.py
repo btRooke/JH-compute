cc99a4b72fbecffcf395a0013ab84cf3e57107bf
Sean-AP <seanapatterson@btinternet.com>
2023-03-05 11:01:03 +0000

    REPL uses stored opponent (it's not very good)

--- libs/connect4/src/connect4/repl.py
+++ libs/connect4/src/connect4/repl.py
@@ -3,32 +3,25 @@ from connect4 import Board, Result
 import trainer
 
 if __name__ == "__main__":
-    # Train a model
-    player1 = Player()
-    player2 = Player()
-
-    trainer.train(1000, player1, player2, 0.9998)
-    wins = trainer.compete(100, player1, player2)
-
-    print(wins)
-    opponent = player1 if wins[0] > wins[1] else player2
-    opponent.write("opponent")
+    opponent = Player("opponent")
     opp_turn = False
 
-    # Play against it
     board = Board()
 
     while True:
+        print(board.state)
+
         if opp_turn:
             # Don't use any randomness - we want to see what the models can do by themselves
             action = opponent.act(board, 0)
 
         else:
-            action = input("Enter a move: ")
+            action = int(input("Enter a move: "))
 
             while not board.move(action):
-                action = input("Enter a move: ")
+                action = int(input("Enter a move: "))
                 
+        opp_turn = not opp_turn
         victory_state = board.check_over(action)
 
         if victory_state:
